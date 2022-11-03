941e9636cbd5de2ed3479c87b53bfd88be249c2a
nadaS90 <engnada_45@outlook.com>
2022-11-03 13:11:23 +0200

    First commit : oppen "App" by click on the btb.

---
+++ src/test/java/FirstAndroidTest.java
@@ -0,0 +1,44 @@
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FirstAndroidTest
+{
+    AppiumDriver driver;
+
+    // this will run brfore test
+    @BeforeTest
+    public  void setup() throws MalformedURLException {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("platformVersion", "11.0");
+        caps.setCapability("deviceName", "Android Emulator 1");
+        caps.setCapability("app", System.getProperty("user.dir")+"/apps/ApiDemos-debug.apk");
+
+        // add appium server
+        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
+    }
+
+    @Test
+    public void click_App_Button()
+    {
+        driver.findElement(new AppiumBy.ByAccessibilityId("App")).click();
+    }
+
+    @AfterTest
+    public void teatDown()
+    {
+        if (null != driver)
+        {
+            driver.quit();
+        }
+    }
+}
